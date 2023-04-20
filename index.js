e8a78c84b4c1e0763a120b3ab335b5b7ca236a51
Jordan Jones <jmjones2463@gmail.com>
2023-04-20 12:01:30 -0700

    perf(ce): implement ce naming convention #246

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-export * from './dist/auro-input';
+import { AuroInput } from './src/auro-input.js';
+
+/**
+ * Register Custom Element.
+ * @param {Object} name - Name to use for custom element.
+ * @returns {void}
+ */
+export function registerComponent(name) {
+  // alias definition
+  if (!customElements.get(name)) {
+    customElements.define(name, class extends AuroInput {});
+  }
+}
