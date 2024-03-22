eac2f43b397bc9000c7a5272f251e3f1789cd025
Jason Baker <jason@capsule42.com>
2024-03-22 16:05:35 -0700

    feat(tokens): add customizable tokens for icons

--- src/auro-input.js
+++ src/auro-input.js
@@ -3,9 +3,9 @@
 
 // ---------------------------------------------------------------------
 
-/* eslint-disable complexity */
+/* eslint-disable complexity, lit/binding-positions, lit/no-invalid-html */
 
-import { html } from "lit";
+import { html } from 'lit/static-html.js';
 import { repeat } from 'lit/directives/repeat.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
@@ -55,12 +55,12 @@ export class AuroInput extends BaseInput {
                This should be cleaned up when auro-icon issue #31 is resolved. -->
             ${this.inputIconName
             ? repeat([this.inputIconName], (val) => val, (name) => html`
-              <auro-icon class="accentIcon" category="payment" name="${name}" part="accentIcon" customColor></auro-icon>
+              <${this.iconTag} class="accentIcon" category="payment" name="${name}" part="accentIcon" customColor></${this.iconTag}>
             `) : undefined
             }
 
             ${this.type === 'month-day-year' || this.type === 'month-year' || this.type === 'year-month-day' || this.type === 'month-fullYear'
-            ? html`<auro-icon class="accentIcon" category="interface" name="calendar" part="accentIcon" customColor></auro-icon>`
+            ? html`<${this.iconTag} class="accentIcon" category="interface" name="calendar" part="accentIcon" customColor></${this.iconTag}>`
             : undefined
             }
           </div>
@@ -100,7 +100,7 @@ export class AuroInput extends BaseInput {
           ?hasValue="${this.hasValue}">
           ${this.validity !== undefined && this.validity !== 'valid' ? html`
             <div class="notification alertNotification">
-              ${this.alertSvg}
+              <${this.iconTag} category="alert" name="error-stroke" error></${this.iconTag}>
             </div>
           ` : undefined}
           ${this.hasValue ? html`
@@ -111,7 +111,11 @@ export class AuroInput extends BaseInput {
                   tabindex="-1"
                   @click="${this.handleClickShowPassword}"
                   class="notificationBtn">
-                  ${this.showPassword ? this.hidePassword : this.viewPassword}
+                  ${this.showPassword ? html`
+                    <${this.iconTag} category="interface" name="hide-password-stroke"></${this.iconTag}>
+                  ` : html`
+                    <${this.iconTag} category="interface" name="view-password-stroke"></${this.iconTag}>
+                  `}
                 </button>
               </div>
             ` : undefined}
@@ -121,7 +125,9 @@ export class AuroInput extends BaseInput {
                 aria-hidden="true"
                 tabindex="-1"
                 @click="${this.handleClickClear}">
-                ${this.readonly ? undefined : this.closeSvg}
+                ${this.readonly ? undefined : html`
+                  <${this.iconTag} category="interface" name="x-sm"></${this.iconTag}>
+                `}
               </button>
             </div>
           ` : undefined}
