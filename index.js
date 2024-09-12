632e17c183fcbad4a9e2f3a7e91dc35112fc04f8
Jordan Jones <jmjones2463@gmail.com>
2024-09-12 12:56:11 -0700

    perf: refactor how the component registers the custom element

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 import { AuroInput } from './src/auro-input.js';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
-export function registerComponent(name) {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroInput {});
-  }
-}
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
+
+RuntimeUtils.default.prototype.registerComponent('auro-icon', AuroInput);
